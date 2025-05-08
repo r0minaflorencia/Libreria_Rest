ba043cd26bba81c777987e3c05cee7f7387fa13a
Romina Avalos <rflorencia.avalos@gmail.com>
2025-05-08 13:38:08 -0300

    add enviroments

--- src/main/java/com/libreria/DemoApplication.java
+++ src/main/java/com/libreria/DemoApplication.java
@@ -3,10 +3,20 @@ package com.libreria;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+
+		// Pasarlas como System env para que Spring las use
+		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("DB_URL"));
+		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
