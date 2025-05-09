444398dd04848b9fb90e14ff6e127448bab5e684
Romina Avalos <rflorencia.avalos@gmail.com>
2025-05-09 18:25:59 -0300

    update properties

--- src/main/java/com/libreria/DemoApplication.java
+++ src/main/java/com/libreria/DemoApplication.java
@@ -3,20 +3,10 @@ package com.libreria;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-
-		Dotenv dotenv = Dotenv.load();
-
-		// Inyectar manualmente las variables como variables de entorno del sistema
-		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
-		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
-		System.setProperty("DB_USER", dotenv.get("DB_USER"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
 		SpringApplication.run(DemoApplication.class, args);
 	}
