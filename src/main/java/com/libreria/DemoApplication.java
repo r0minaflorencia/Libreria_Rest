2f0643e2e20c44e93cc43ded84d3683771e0219c
Romina Avalos <rflorencia.avalos@gmail.com>
2025-05-15 14:14:39 -0300

    add enviroments

--- src/main/java/com/libreria/DemoApplication.java
+++ src/main/java/com/libreria/DemoApplication.java
@@ -3,11 +3,22 @@ package com.libreria;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 
+		// 1. Cargar .env antes de que Spring se inicie
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./")
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+		// 2. Cargar Spring
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
