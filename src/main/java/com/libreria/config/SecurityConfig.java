ba3d1e9c1d95381db603a104b4ef743071751434
Romina Avalos <rflorencia.avalos@gmail.com>
2025-06-11 21:20:10 -0300

    fix services

--- src/main/java/com/libreria/config/SecurityConfig.java
+++ src/main/java/com/libreria/config/SecurityConfig.java
@@ -1,8 +1,13 @@
 package com.libreria.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,14 +16,34 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.libreria.services.UsuarioServiceImpl;
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
-        
+
+        @Autowired
+        private UsuarioServiceImpl usuarioService; // o tu UserDetailsService
+
+        @Bean
+        public AuthenticationManager authenticationManager(
+                        AuthenticationConfiguration authenticationConfiguration) throws Exception {
+                return authenticationConfiguration.getAuthenticationManager();
+        }
+
         @Bean
         public PasswordEncoder passwordEncoder() {
                 return new BCryptPasswordEncoder();
+        }
+
+        @Bean
+        public AuthenticationProvider authenticationProvider() {
+                DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+                authProvider.setUserDetailsService(usuarioService); // Asegúrate que UsuarioService implemente
+                                                                    // UserDetailsService
+                authProvider.setPasswordEncoder(passwordEncoder());
+                return authProvider;
         }
 
         @Bean
